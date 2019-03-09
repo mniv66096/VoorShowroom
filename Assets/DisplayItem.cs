c4f02b906846055297f0f15c69c3686a586e00da
Siyuan.Q <siyuan.qiu1@gmail.com>
2019-03-09 00:22:26 -0500

    Added base classes for showing the linesheet on gaze

---
+++ Assets/DisplayItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class DisplayItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField] GameObject _linesheetPrefab;
+    [SerializeField] UnityEvent PointerClickedAction;
+
+    public LinesheetTemplate Linesheet { get; private set; }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PointerClickedAction?.Invoke();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Linesheet.Show(0.5f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Linesheet.Hide(1.0f, 0.5f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Linesheet = Instantiate(_linesheetPrefab).GetComponent<LinesheetTemplate>();
+    }
+}
---
+++ Assets/LinesheetTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LinesheetTemplate : MonoBehaviour
+{
+    private CanvasGroup _canvasGroup;
+
+    private void Awake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _canvasGroup.alpha = 0;
+    }
+
+    public void Show(float fadeDuration)
+    {
+        DOTween.Kill(_canvasGroup);
+        _canvasGroup.DOFade(1.0f, fadeDuration);
+    }
+
+    public void Hide(float fadeDuration, float delay)
+    {
+        DOTween.Kill(_canvasGroup);
+        DOVirtual.DelayedCall(delay, () => { _canvasGroup.DOFade(0.0f, fadeDuration); })
+            .SetTarget(_canvasGroup);
+    }
+
+
+}
